13f9f5f049d460a9c0badd48ee91f25994eced35
suganyasugi3 <Suganyasugi334@gmail.com>
2021-11-11 09:36:39 +0530

    Password Nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('mobile')->nullable();
             $table->date('dob')->nullable();
             $table->string('country')->nullable();
